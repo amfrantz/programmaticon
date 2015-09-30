7d2d1b298dec720bd89e58cf84dd5453b1e48a2c
andrewf <andrewf@porch.com>
2015-09-30 15:47:15 -0700

    extract constants. programmatically position user initials.

--- src/main/java/com/github/amfrantz/programmaticon/Programmaticon.java
+++ src/main/java/com/github/amfrantz/programmaticon/Programmaticon.java
@@ -4,6 +4,7 @@ import javax.imageio.ImageIO;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
@@ -15,8 +16,16 @@ import java.util.Random;
 
 public class Programmaticon {
 
-    private static final int WIDTH = 227;
-    private static final int HEIGHT = 227;
+    private static final int CANVAS_WIDTH = 227;
+    private static final int CANVAS_HEIGHT = 227;
+
+    private static final int BODY_WIDTH_MIN = 125;
+    private static final int BODY_WIDTH_MAX = 160;
+    private static final int BODY_HEIGHT_MIN = 200;
+    private static final int BODY_HEIGHT_MAX = 250;
+
+    private static final int HEAD_SIZE_MIN = 100;
+    private static final int HEAD_SIZE_MAX = 115;
 
     private static Color cotton = new Color(243, 244, 246);
 
@@ -28,7 +37,7 @@ public class Programmaticon {
     private static final List<Color> availableColors = Arrays.asList(gold, teal, ruby, blue);
 
     public static void main(String[] args) throws IOException {
-        BufferedImage composite = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage composite = new BufferedImage(CANVAS_WIDTH, CANVAS_HEIGHT, BufferedImage.TYPE_INT_ARGB);
 
         Graphics2D graphics = (Graphics2D) composite.getGraphics();
         graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
@@ -37,28 +46,34 @@ public class Programmaticon {
         // background
         Color backgroundColor = getRandomColor(null);
         graphics.setColor(backgroundColor);
-        graphics.fillRect(0, 0, WIDTH, HEIGHT);
+        graphics.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
         // body (make sure it isn't the same as the background
         Color bodyColor = getRandomColor(backgroundColor);
         graphics.setColor(bodyColor);
-        int bodyWidth = getRandomInRange(125, 160);
-        int bodyHeight = getRandomInRange(200, 250);
-        int bodyX = WIDTH / 2 - bodyWidth / 2;
-        int bodyY = HEIGHT - bodyHeight / 2;
+        int bodyWidth = getRandomInRange(BODY_WIDTH_MIN, BODY_WIDTH_MAX);
+        int bodyHeight = getRandomInRange(BODY_HEIGHT_MIN, BODY_HEIGHT_MAX);
+        int bodyX = CANVAS_WIDTH / 2 - bodyWidth / 2;
+        int bodyY = CANVAS_HEIGHT - bodyHeight / 2;
 
         graphics.fillOval(bodyX, bodyY, bodyWidth, bodyHeight);
 
         // head
         graphics.setColor(cotton);
-        int headSize = getRandomInRange(100, 115);
-        int headX = WIDTH / 2 - headSize / 2;
-        int headY = HEIGHT - bodyHeight / 2 - ((headSize / 4) * 3);
+        int headSize = getRandomInRange(HEAD_SIZE_MIN, HEAD_SIZE_MAX);
+        int headX = CANVAS_WIDTH / 2 - headSize / 2;
+        int headY = CANVAS_HEIGHT - bodyHeight / 2 - ((headSize / 4) * 3);
         graphics.fillOval(headX, headY, headSize, headSize);
 
         // initials
+        String initials = getUserInitials();
         graphics.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 65));
-        graphics.drawString(getUserInitials(), 74, 210);
+        FontMetrics fm = graphics.getFontMetrics();
+        int initialsWidth = fm.stringWidth(initials);
+        int initialsHeight = fm.getAscent();
+        int initialsX = CANVAS_WIDTH / 2 - initialsWidth / 2;
+        int initialsY = headY + headSize + (CANVAS_HEIGHT - (headY + headSize)) / 2 + initialsHeight / 2;
+        graphics.drawString(initials, initialsX, initialsY);
 
         ImageIO.write(composite, "png", new File("composite.png"));
     }
